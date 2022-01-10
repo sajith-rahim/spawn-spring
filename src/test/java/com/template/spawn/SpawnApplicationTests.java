5e051e7c3e5025812a8e5c0d34df4f2025dae320
sajith.rahim <sajith.rahim@outlook.com>
2022-01-11 01:26:52 +0530

    Create SpawnApplicationTests.java

---
+++ src/test/java/com/template/spawn/SpawnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.template.spawn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpawnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
